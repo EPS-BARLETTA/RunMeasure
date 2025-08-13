5b973c1096bc77c0279eb6014668f40e7c0c9fc8
EPS-BARLETTA <j.barletta@vauban.lu>
2025-08-13 18:44:32 +0200

    Delete qr.js

--- qr.js
+++
@@ -1,9 +0,0 @@
-
-window.makeQRCode = (container, payload)=>{
-  if(!container) return;
-  container.innerHTML='';
-  try{
-    const text = JSON.stringify(payload);
-    new QRCode(container,{text,width:220,height:220});
-  }catch(e){ console.error(e); container.textContent='Erreur QR'; }
-};

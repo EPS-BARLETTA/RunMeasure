82cd338f2868557085898827cf4926967a610b74
EPS-BARLETTA <j.barletta@vauban.lu>
2025-08-13 21:28:14 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-
-const CACHE='chrono-eps-v6';const ASSETS=['./','./index.html','./eleve.html','./projection.html','./assets/styles.css','./assets/app.js','./assets/manifest.webmanifest'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
-self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.origin===location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))}});
